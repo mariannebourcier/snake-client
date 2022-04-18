3aa428c0eac4ad248a601ffccbce52d1acdbf378
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-18 14:17:09 -0600

    client.js file added and exported function

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.1.67',
-    port: '50541'
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require('./client');
 
 
 
-console.log("Connecting ...");
-connect();
